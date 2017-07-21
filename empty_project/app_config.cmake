caa1a18527ba6f2c7443b6d7473d34a6507db458
Keehong Youn <younkeehong@gmail.com>
2017-07-20 22:25:00 -0700

    adding empty project

---
+++ empty_project/app_config.cmake
@@ -0,0 +1,9 @@
+# list your app files here
+set(app_files_list
+	main.cpp
+)
+
+# where is al_lib?
+# this doesn't get used if app is run via run script in Linux or MacOS
+# `app_path` holds path to current project
+set(user_al_path ${app_path}/../al_lib)
