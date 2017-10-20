c8d8b005a0fa4ace735048997f12ee10aa50d187
Keehong Youn <younkeehong@gmail.com>
2017-10-19 20:56:52 -0700

    fix breaking build

--- include/al/core/graphics/al_RenderManager.hpp
+++ include/al/core/graphics/al_RenderManager.hpp
@@ -101,10 +101,6 @@ public:
 
 class RenderManager {
 public:
-
-	RenderManager(Window* w): mWindowPtr(w) {}
-
-
   
   void setClearColor(float r, float g, float b, float a = 1);
   void clearColor(int drawbuffer=0);
