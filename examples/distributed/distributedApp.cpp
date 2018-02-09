a042ed9125556a73eb11c877b7c6a25db4286b6d
sphere <sphere@spherez05.mat.ucsb.edu>
2018-02-09 14:27:26 -0800

    Further refinements

--- examples/distributed/distributedApp.cpp
+++ examples/distributed/distributedApp.cpp
@@ -23,7 +23,59 @@ Andres Cabrera 2/2018
 #include "al/core/app/al_DistributedApp.hpp"
 using namespace al;
 
+float *create_rand_nums(int num_elements) {
+  float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
+  assert(rand_nums != NULL);
+  int i;
+  for (i = 0; i < num_elements; i++) {
+    rand_nums[i] = (rand() / (float)RAND_MAX);
+  }
+  return rand_nums;
+}
+
+
+class DistributedExampleApp : public DistributedApp {
+public:
+    virtual void simulate() override {
+        unsigned long num_elements_per_proc = 1e6;
+        float *rand_nums = NULL;
+        rand_nums = create_rand_nums(num_elements_per_proc);
+
+        // Sum the numbers locally
+        float local_sum = 0;
+
+        for (auto i = 0ul; i < num_elements_per_proc; i++) {
+          local_sum += rand_nums[i];
+        }
+
+        // Print the random numbers on each process
+        printf("Local sum for process %d - %f, avg = %f\n",
+               world_rank, local_sum, local_sum / num_elements_per_proc);
+
+        // Reduce all of the local sums into the global sum
+        float global_sum;
+        MPI_Reduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, 0,
+                   MPI_COMM_WORLD);
+
+        // Print the result
+        if (world_rank == 0) {
+          printf("Total sum = %f, avg = %f\n", global_sum,
+                 global_sum / (world_size * num_elements_per_proc));
+        }
+    }
+
+};
 
 int main(){
-    DistributedApp app;
+    DistributedExampleApp app;
+    app.fps(1);
+    app.startFPS();
+    app.print();
+    for (int i = 0; i < 10; i++) {
+        if (app.isMaster()) {
+            std::cout << " Run " << i << " ---------------" <<std::endl;
+        }
+        app.simulate();
+    }
+    return 0;
 }
--- include/al/core/app/al_DistributedApp.hpp
+++ include/al/core/app/al_DistributedApp.hpp
@@ -38,27 +38,30 @@ public:
     DistributedApp() {
         MPI_Init(NULL, NULL);
 
-        // Get the number of processes
-        int world_size;
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-        // Get the rank of the process
-        int world_rank;
         MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-        // Get the name of the processor
-        char processor_name[MPI_MAX_PROCESSOR_NAME];
-        int name_len;
         MPI_Get_processor_name(processor_name, &name_len);
-
-        std::cout << "Processor: " << processor_name
-                  << " Rank: " << world_rank
-                  << " Of: " << world_size << std::endl;
     }
 
     ~DistributedApp() {
         MPI_Finalize();
     }
+
+    void print() {
+        std::cout << "Processor: " << processor_name
+                  << " Rank: " << world_rank
+                  << " Of: " << world_size << std::endl;
+    }
+
+    bool isMaster() {return world_rank == 0;}
+
+    virtual void simulate() = 0;
+
+    int world_size;
+    int world_rank;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
 
     Graphics mGraphics;
     AppEventHandler eventHandler {this};
