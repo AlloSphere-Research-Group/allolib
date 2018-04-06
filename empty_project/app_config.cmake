1b5f4ef606dd5c7f95dc14797cca62a0b03d8683
Keehong Youn <younkeehong@gmail.com>
2018-04-06 00:55:59 -0700

    update empty project template

--- empty_project/app_config.cmake
+++ empty_project/app_config.cmake
@@ -1,16 +1,15 @@
-# WINDOWS is set for Windows, MACOS for MacOS, LINUX for Linux
-# ${home_path} holds user's home path
-# ${app_path} holds path to current project
+# allolib location
+set(allolib_directory ../allolib)
 
 # list your app files here
 set(app_files_list
 	main.cpp
 )
 
-# other directories to include?
+# other directories to include
 set(app_include_dirs
 )
 
-# other libraries to link?
+# other libraries to link
 set(app_link_libs
 )
--- empty_project/main.cpp
+++ empty_project/main.cpp
@@ -1,54 +1,27 @@
 #include "al/core.hpp"
 
 using namespace al;
-using namespace std;
 
-class MyApp : public App {
-public:
-  Mesh mesh;
-  osc::Recv server {11117};
+struct MyApp : App
+{
 
-  void onCreate() {
-    addIcosahedron(mesh);
+  void onCreate() override {
 
-	server.handler(*this);
-	server.start();
   }
 
-  void onAnimate(double dt) {
+  void onAnimate(double dt) override {
     
   }
 
-  void onDraw(Graphics& g) {
+  void onDraw(Graphics& g) override {
     g.clear(0, 0, 0);
-    g.color(1, 0, 0);
-    g.polygonMode(Graphics::LINE);
-    g.draw(mesh);
   }
 
-  void onSound(AudioIOData& io) {
-    static double phase {0};
-    double phaseIncrement = 440.0 / io.framesPerSecond();
-
-    while(io()){
-      phase += phaseIncrement;
-      if(phase > 1) phase -= 1;
-      float out = 0.05 * cos(phase * M_2PI);
-      io.out(0) = out;
-      io.out(1) = out;
-    }
-  }
-
-  void onMessage(osc::Message& m)
-  {
-  }
 };
 
-int main() {
+int main()
+{
   MyApp app;
-  app.fps(60);
-  app.title("empty project");
-  app.dimensions(500, 500);
-  app.initAudio();
+  app.dimensions(512, 512);
   app.start();
 }
