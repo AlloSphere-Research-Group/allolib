32f5ed8740bcd6670960a7c2061abbd5b160839b
Keehong Youn <younkeehong@gmail.com>
2018-05-10 02:39:49 -0700

    add scissoring example

---
+++ examples/graphics/scissor.cpp
@@ -0,0 +1,156 @@
+#include "al/core.hpp"
+
+using namespace al;
+
+Matrix4f getLookAt(const Vec3f& ux, const Vec3f& uy, const Vec3f& uz, const Vec3f& p) {
+  return Matrix4f(
+     ux[0], ux[1], ux[2], -(ux.dot(p)),
+     uy[0], uy[1], uy[2], -(uy.dot(p)),
+     uz[0], uz[1], uz[2], -(uz.dot(p)),
+         0,     0,     0,       1
+  );
+}
+
+Matrix4f getLookAt(const Vec3f& eyePos, const Vec3f& at, const Vec3f& up) {
+  Vec3f z = (eyePos - at).normalize();
+  Vec3f x = cross(up, z).normalize();
+  Vec3f y = cross(z, x).normalize();
+  return getLookAt(x, y, z, eyePos);
+}
+
+struct MyApp : App {
+  Mesh mesh;
+  Mesh axis;
+  Light light;
+  double angle1, angle2;
+
+  void onCreate() override {
+    angle1 = angle2 = 0;
+    int Nv = addIcosahedron(mesh);
+    for (int i = 0; i < Nv; ++i) {
+      float f = float(i) / Nv;
+      mesh.color(HSV(f, 1, 1));
+    }
+    mesh.decompress();
+    mesh.generateNormals();
+
+    axis.primitive(Mesh::LINES);
+
+    axis.vertex(-2, 0, 0);
+    axis.vertex(5, 0, 0);
+    axis.color(1, 0, 0);
+    axis.color(1, 0, 0);
+
+    axis.vertex(0, -2, 0);
+    axis.vertex(0, 5, 0);
+    axis.color(0, 1, 0);
+    axis.color(0, 1, 0);
+
+    axis.vertex(0, 0, -2);
+    axis.vertex(0, 0, 5);
+    axis.color(0, 0, 1);
+    axis.color(0, 0, 1);
+
+    nav().pos(0, 0, 10).faceToward({0, 0, 0}, {0, 1, 0});
+
+    light.pos(0, 10, 0);
+  }
+
+  void onAnimate(double dt) override {
+    angle1 += 0.02;
+    angle2 += 0.1;
+  }
+
+  void onDraw(Graphics& g) override {
+    g.clear(0, 0, 0);
+
+    g.depthTesting(true);
+    g.light(light);
+
+    auto draw_mesh = [this, &g](){
+
+      g.lighting(false);
+      g.meshColor();
+      g.draw(axis);
+
+      g.rotate(angle1, 0, 1, 0);
+      g.rotate(angle2, 1, 0, 0);
+
+      g.lighting(true);
+      g.meshColor();
+      g.polygonMode(Graphics::FILL);
+      g.draw(mesh);
+
+      g.scale(1.01);
+      g.color(0);
+      g.polygonMode(Graphics::LINE);
+      g.draw(mesh);
+    };
+
+    int w = fbWidth() / 2;
+    int h = fbHeight() / 2;
+    g.scissorTest(true);
+
+    {
+      g.viewport(0, 0, w, h);
+      g.scissor(0, 0, w, h);
+      g.clear(0.9, 0.9, 0.5);
+      g.pushMatrix();
+      g.pushViewMatrix();
+      g.viewMatrix(getLookAt({8, 0, 0}, {0, 0, 0}, {0, 1, 0}));
+      draw_mesh();
+      g.popViewMatrix();
+      g.popMatrix();
+    }
+
+    {
+      g.viewport(w, 0, w, h);
+      g.scissor(w, 0, w, h);
+      g.clear(0.5, 0.9, 0.5);
+      g.pushMatrix();
+      g.pushViewMatrix();
+      g.viewMatrix(getLookAt({0, 8, 0}, {0, 0, 0}, {0, 0, 1}));
+      draw_mesh();
+      g.popViewMatrix();
+      g.popMatrix();
+    }
+
+    {
+      g.viewport(0, h, w, h);
+      g.scissor(0, h, w, h);
+      g.clear(0.5, 0.9, 0.9);
+      g.pushMatrix();
+      g.pushViewMatrix();
+      g.viewMatrix(getLookAt({0, 0, 8}, {0, 0, 0}, {1, 0, 0}));
+      draw_mesh();
+      g.popViewMatrix();
+      g.popMatrix();
+    }
+
+    {
+      g.viewport(w, h, w, h);
+      g.scissor(w, h, w, h);
+      g.clear(0.9, 0.5, 0.9);
+      g.pushMatrix();
+      g.pushViewMatrix();
+      g.viewMatrix(getLookAt({8, 8, 8}, {0, 0, 0}, {0, 1, 0}));
+      draw_mesh();
+      g.popViewMatrix();
+      g.popMatrix();
+    }
+
+    g.viewport(0, 0, fbWidth(), fbHeight());
+    g.scissorTest(false);
+
+    g.pushMatrix();
+    draw_mesh();
+    g.popMatrix();
+  }
+
+};
+
+int main()
+{
+	MyApp app;
+	app.start();
+}
