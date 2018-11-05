cbcea568a16616a3d4fa1275786df7ef68affe63
Andres Cabrera <mantaraya36@gmail.com>
2018-11-05 11:07:07 -0800

    Improved user_flags example

--- examples/user_flags/flags.cmake
+++ examples/user_flags/flags.cmake
@@ -1,17 +1,24 @@
-# other directories to include
-set(app_include_dirs
+
+message("Flags. cmake")
+
+# other directories to include. You can use relative paths to the
+# source file being built.
+set(app_include_dirs extra
 )
 
 # other libraries to link
 set(app_link_libs
 )
 
-# definitions
+# definitions. Prepend -D to any defines
 set(app_definitions
+    -DUSE_COLOR
 )
 
 # compile flags
+# This flag ignores unused variable warning. You probably don't want to do this...
 set(app_compile_flags
+    -Wno-unused-variable
 )
 
 # linker flags, with `-` in the beginning
--- examples/user_flags/main.cpp
+++ examples/user_flags/main.cpp
@@ -11,50 +11,36 @@ Keehong Youn, 2017
 */
 
 #include "al/core.hpp"
+
+// This will be picked up in the subdirectory because it has bee added in
+// flags.cmake to the app_include_dirs variable
+
+#include "drawing.hpp"
+
 using namespace al;
 
 class MyApp : public App {
 public:
 
-	Mesh verts;
-	Mesh verts2 {Mesh::LINES};
+    void onCreate () {
+        drawing.init();
+    }
 
-	void onCreate ()
-	{
-		verts.primitive(Mesh::LINE_STRIP);
-		// Create a sine wave
-		const int N = 128;
-		for(int i=0; i<N; ++i){
-			float f = float(i)/(N-1);
-			verts.vertex(2*f-1, 0.5*sin(f*M_PI*2));
-		}
+    void onAnimate(double dt) {
+        drawing.tick(width(), height());
+    }
+
+    void onDraw (Graphics& g)
+    {
+        // This unused variable will not report a warning because of the
+        // compiler flags passed in app_compile_flags
+        int unused = 42;
+        g.clear(0, 0, 0);
+        drawing.draw(g);
+    }
 
 
-	}
-
-	void onAnimate(double dt)
-	{
-		const int N = 256;
-		const float w = width();
-		const float h = height();
-		verts2.reset();
-		for (int i = 0; i < N; i += 1) {
-			float t = float(i) / (N - 1);
-			verts2.vertex(w * t, h * (0.5 + 0.5 * cos(t * M_2PI)));
-			verts2.color(t, 1 - t, 0);
-		}
-	}
-
-	void onDraw (Graphics& g)
-	{
-		g.clear(0, 0, 0);
-		g.camera(Viewpoint::IDENTITY);
-		g.color(1, 1, 1);
-		g.draw(verts);
-		g.camera(Viewpoint::ORTHO_FOR_2D);
-		g.meshColor();
-		g.draw(verts2);
-	}
+    Drawing drawing;
 };
 
 int main () {
