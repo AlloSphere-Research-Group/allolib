f2a836ac96179d8b7551def40485c83665067b2a
Andres Cabrera <mantaraya36@gmail.com>
2019-02-18 11:52:48 -0800

    Minor fix to compensate for missing time before first preset change in SequenceRecorder

--- src/util/ui/al_SequenceRecorder.cpp
+++ src/util/ui/al_SequenceRecorder.cpp
@@ -150,6 +150,13 @@ void SequenceRecorder::recorderFunction(SequenceRecorder *recorder, std::string 
 
                 std::cout << currentTime << ":" << lastPresetTime << ":" << steps[lastPresetIndex].morphTime << std::endl;
             }
+            if (steps.front().type == PresetSequencer::PARAMETER) {
+                PresetSequencer::Step fillerStep;
+                fillerStep.type = PresetSequencer::PARAMETER;
+                fillerStep.presetName = "_";
+                fillerStep.params = {0.0f};
+                steps.push_back(fillerStep);
+            }
             lastPresetIndex = (long) steps.size();
             lastPresetTime = currentTime;
         } else if (recorder->mStepToInsert.type == PresetSequencer::PARAMETER) {
