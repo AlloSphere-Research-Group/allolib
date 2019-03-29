67dedd165c2b59d12ce8f041d099bfccdcd39467
Andres Cabrera <andres@atari>
2019-03-29 15:03:11 -0700

    Fix compilation issue
    # Conflicts:
    #	include/al/util/ui/al_ParameterGUI.hpp

--- include/al/util/ui/al_ParameterGUI.hpp
+++ include/al/util/ui/al_ParameterGUI.hpp
@@ -316,14 +316,14 @@ public:
 
     void createBundle(uint8_t bundleSize) {
         for (uint8_t i = 0; i < bundleSize; i++) {
-            auto voice = mSequencer.synth()->getVoice<VoiceType>();
+            auto voice = mSequencer.synth().template getVoice<VoiceType>();
             auto bundle = std::make_shared<ParameterBundle>(demangle(typeid (mControlVoice).name()));
             for(auto *param: voice->triggerParameters()) {
                 *bundle << param;
             }
             mBundles.push_back(bundle);
             mBundleGui << *bundle;
-            mSequencer.synth()->triggerOn(voice);
+            mSequencer.synth().triggerOn(voice);
         }
     }
 
