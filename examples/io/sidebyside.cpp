ccfd0ad5f25729ccd4dc77704eaa89b1f44f763a
Keehong Youn <younkeehong@gmail.com>
2019-05-28 02:52:11 -0700

    simple example for side by side stereo setup

---
+++ examples/io/sidebyside.cpp
@@ -0,0 +1,69 @@
+#include "al/core.hpp"
+#include <iostream>
+
+using namespace al;
+
+struct MyApp : App
+{
+    Mesh mesh;
+    double phase = 0;
+
+    void onCreate() override
+    {
+        addTetrahedron(mesh);
+        lens().near(0.1).far(25).fovy(45);
+        nav().pos(0,0,4);
+        nav().quat().fromAxisAngle(0.*M_2PI, 0,1,0);
+    }
+
+    void onAnimate (double dt) override
+    {
+        double period = 10000;
+        phase += dt / period;
+        if(phase >= 1.) phase -= 1.;
+    }
+
+    void onDraw (Graphics& g) override
+    {
+        /*  Left
+        */
+        glViewport(0, 0, fbWidth()/2, fbHeight());
+        glEnable(GL_SCISSOR_TEST);
+        glScissor(0, 0, fbWidth()/2, fbHeight());
+
+        g.clear(1, 0, 0);
+        g.polygonMode(Graphics::LINE);
+        g.pushMatrix();
+        g.rotate(phase*360, 0,1,0);
+        g.color(1);
+        g.draw(mesh);
+        g.popMatrix();
+
+        /*  Right
+        */
+        glViewport(fbWidth()/2, 0, fbWidth()/2, fbHeight());
+        glEnable(GL_SCISSOR_TEST);
+        glScissor(fbWidth()/2, 0, fbWidth()/2, fbHeight());
+
+        g.clear(0, 0, 1);
+        g.polygonMode(Graphics::LINE);
+        g.pushMatrix();
+        g.rotate(phase*360, 0,1,0);
+        g.color(1);
+        g.draw(mesh);
+        g.popMatrix();
+
+        /*  Reset settings
+        */
+        glViewport(0, 0, fbWidth(), fbHeight());
+        glDisable(GL_SCISSOR_TEST);
+    }
+};
+
+int main ()
+{
+    MyApp app;
+    app.dimensions(600, 400);
+    app.decorated(false);
+    app.start();
+}
