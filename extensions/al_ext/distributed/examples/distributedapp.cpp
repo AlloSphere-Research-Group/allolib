e2742725bf35f10fc0f7af79e12fbfe6b7f43e2a
Andres Cabrera <andres@atari>
2019-07-03 17:21:05 -0700

    Fixed distributed app example

--- extensions/al_ext/distributed/examples/distributedapp.cpp
+++ extensions/al_ext/distributed/examples/distributedapp.cpp
@@ -13,48 +13,12 @@ struct State {
   float value;
 };
 
-class DistributedApp : public BaseCompositeApp {
-public:
-  DistributedApp(bool primary = true) : BaseCompositeApp() {
-
-    // State will be same memory for local, but will be synced on the network for separate instances
-
-    mOpenGLGraphicsDomain->removeSubDomain(simulationDomain());
-
-    // Replace Simulation domain with state simulation domain
-    mSimulationDomain = mOpenGLGraphicsDomain->newSubDomain<StateSimulationDomain<State>>(true);
-
-    mPrimary = primary;
-    if (mPrimary) {
-      std::cout << "Running primary" << std::endl;
-      auto sender = std::static_pointer_cast<StateSimulationDomain<State>>(mSimulationDomain)->addStateSender("state");
-      sender->configure(10101);
-    } else {
-      std::cout << "Running REPLICA" << std::endl;
-      auto receiver = std::static_pointer_cast<StateSimulationDomain<State>>(mSimulationDomain)->addStateReceiver("state");
-      receiver->configure(10101);
-      mSimulationDomain->disableProcessingCallback(); // Replicas won't call onAnimate()
-    }
-  }
-
-  State &state() {
-    return std::static_pointer_cast<StateSimulationDomain<State>>(mSimulationDomain)->state();
-  }
-
-  void setTitle(std::string title) {
-    graphicsDomain()->app.title(title);
-  }
-
-private:
-  bool mPrimary;
-};
-
-class MyApp: public DistributedApp {
+class MyApp: public DistributedAppWithState<State> {
 public:
   gam::Sine<> mOsc {440};
   gam::Sine<> mOsc2 {1};
 
-  MyApp(bool primary = true) : DistributedApp(primary) {
+  MyApp(uint16_t rank = 0) : DistributedAppWithState<State>(rank) {
     mOsc.domain(*audioDomain());
     mOsc2.domain(*graphicsDomain());
 
@@ -83,7 +47,7 @@ public:
 int main(int argc, char *argv[])
 {
   if (argc > 1 || !osc::Recv::portAvailable(9010, "0.0.0.0")) { // Run replica
-    MyApp app(false);
+    MyApp app(1);
     app.setTitle("REPLICA");
     app.start();
 
