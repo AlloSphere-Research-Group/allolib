c36ad4003a56d201b7cfeba88c4ccce0c095da48
Andres Cabrera <mantaraya36@gmail.com>
2019-10-02 16:36:23 -0700

    Fixes to examples

--- examples/ui/app_server.cpp
+++ examples/ui/app_server.cpp
@@ -2,9 +2,9 @@
 #include "al/app/al_App.hpp"
 #include "al/graphics/al_Shapes.hpp"
 
+#include "al/ui/al_ControlGUI.hpp"
 #include "al/ui/al_Parameter.hpp"
 #include "al/ui/al_ParameterServer.hpp"
-#include "al/ui/al_ControlGUI.hpp"
 
 using namespace al;
 
@@ -13,65 +13,61 @@ using namespace al;
 // be picked up by this server.
 
 struct MyApp : public App {
-    Parameter X {"X", "", 0.0f, "", -1.0f, 1.0f};
-    Parameter Y {"Y", "", 0.0f, "", -1.0f, 1.0f};
+  Parameter X{"X", "", 0.0f, "", -1.0f, 1.0f};
+  Parameter Y{"Y", "", 0.0f, "", -1.0f, 1.0f};
 
-    ControlGUI gui;
+  ControlGUI gui;
 
-    Mesh mesh;
+  Mesh mesh;
 
-    MyApp() {
-        navControl().disable(); // Disable Keyboard and mouse navigaion
+  void onCreate() override {
+    // Add Cone shape to mesh
+    addCone(mesh);
+    mesh.primitive(Mesh::LINE_STRIP);
+    // Register parameters with GUI
+    gui << X << Y;
+    // Initialize control GUI
+    gui.init();
+    // Register parameters with App's internal parameter server
+    parameterServer() << X << Y;
+    parameterServer().print();
+    // You can change the port and listening address of the parameter server at
+    // runtime
+    parameterServer().listen(9011, "localhost");
+    parameterServer().print();
+
+    navControl().disable();  // Disable Keyboard and mouse navigaion
+  }
+
+  void onDraw(Graphics& g) override {
+    g.clear();
+    g.pushMatrix();
+    g.translate(X, Y, -8.0);
+    g.draw(mesh);
+    g.popMatrix();
+    gui.draw(g);
+  }
+
+  bool onKeyDown(Keyboard const& k) override {
+    // Use the 'd' and 'a' keys to change the X value
+    // This sends an OSC message that will be receieved by the
+    // parameter server
+    if (k.key() == 'd') {
+      osc::Send sender(parameterServer().serverPort());
+      sender.send("/X", X + 0.1f);
     }
 
-    void onCreate() override {
-        // Add Cone shape to mesh
-        addCone(mesh);
-        mesh.primitive(Mesh::LINE_STRIP);
-        // Register parameters with GUI
-        gui << X << Y;
-        // Initialize control GUI
-        gui.init();
-        // Register parameters with App's internal parameter server
-        parameterServer() << X << Y;
-        parameterServer().print();
-        // You can change the port and listening address of the parameter server at runtime
-        parameterServer().listen(9011, "localhost");
-        parameterServer().print();
+    if (k.key() == 'a') {
+      osc::Send sender(parameterServer().serverPort());
+      sender.send("/X", X - 0.1f);
     }
-
-    void onDraw(Graphics &g) override {
-        g.clear();
-        g.pushMatrix();
-        g.translate(X, Y, -8.0);
-        g.draw(mesh);
-        g.popMatrix();
-        gui.draw(g);
-    }
-
-    bool onKeyDown(Keyboard const& k) override {
-        // Use the 'd' and 'a' keys to change the X value
-        // This sends an OSC message that will be receieved by the
-        // parameter server
-        if (k.key() == 'd') {
-            osc::Send sender(parameterServer().serverPort());
-            sender.send("/X", X + 0.1f);
-        }
-
-        if (k.key() == 'a') {
-            osc::Send sender(parameterServer().serverPort());
-            sender.send("/X", X - 0.1f);
-        }
-        return true;
-    }
-
+    return true;
+  }
 };
 
+int main() {
+  MyApp app;
+  app.start();
 
-int main()
-{
-    MyApp app;
-    app.start();
-
-    return 0;
+  return 0;
 }
