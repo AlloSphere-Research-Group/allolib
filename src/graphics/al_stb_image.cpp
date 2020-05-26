c343e3dbbce6575138b4b916f8105f54aaae9408
Andres Cabrera <mantaraya36@gmail.com>
2020-05-26 10:38:50 -0700

    Reverted change to allow saving of other formats apart from png.

--- src/graphics/al_stb_image.cpp
+++ src/graphics/al_stb_image.cpp
@@ -1,5 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
-#define STBI_ONLY_PNG
+#define STBI_NO_TGA
+#define STBI_NO_PSD
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_GIF
+#define STBI_NO_PNM
 #include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
