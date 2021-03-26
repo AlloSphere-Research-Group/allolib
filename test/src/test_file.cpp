0db0bf93471611884726dd3598452960683a2d54
Andres Cabrera <mantaraya36@gmail.com>
2021-03-26 11:20:59 -0700

    Added more unit tests for File::conformDirectory() and conformPathToOS()

--- test/src/test_file.cpp
+++ test/src/test_file.cpp
@@ -11,6 +11,65 @@ TEST_CASE("Conform directory") {
   std::string conformed = File::conformDirectory(path);
 
   REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
-  //
-  //
+
+  path = "c:\\test\\path\\..\\otherpath\\to\\";
+  conformed = File::conformDirectory(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
+
+  path = "c:\\test\\path/..\\otherpath\\to\\";
+  conformed = File::conformDirectory(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
+
+  // spaces
+  path = "c:\\test\\pa th/..\\other path\\to\\";
+  conformed = File::conformDirectory(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/other path/to/"));
+}
+
+TEST_CASE("Conform path to OS") {
+  std::string path = "c:/test/path/../otherpath/to/";
+  std::string conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
+
+  path = "c:\\test\\path\\..\\otherpath\\to\\";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
+
+  path = "c:\\test\\path/..\\otherpath\\to\\";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/"));
+
+  // spaces
+  path = "c:\\test\\pa th/..\\other path\\to\\";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/other path/to/"));
+
+  // files
+  path = "c:/test/path/../otherpath/to/file";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/file"));
+
+  path = "c:\\test\\path\\..\\otherpath\\to\\file";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/file"));
+
+  path = "c:\\test\\path/..\\otherpath\\to\\file";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/otherpath/to/file"));
+
+  // spaces
+  path = "c:\\test\\pa th/..\\other path\\to\\";
+  conformed = File::conformPathToOS(path);
+
+  REQUIRE(File::isSamePath(conformed, "c:/test/other path/to/file"));
 }
